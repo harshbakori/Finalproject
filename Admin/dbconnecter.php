32de65f45e25d89d6b6bf23bb27a039a6d6e92c6
D3@D53C71 <30556333+harshbakori@users.noreply.github.com>
2020-03-26 13:14:09 +0530

    admin login complit
    remains to add login page details after login

---
+++ Admin/dbconnecter.php
@@ -0,0 +1,9 @@
+<!-- #all set do not change anything  -->
+<!-- I reapete DO NOT CHANGE ANYTHING -->
+<?php
+   define('DB_SERVER', 'localhost:3306');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'finalproject');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
